1c76dc9c29257ffbdcf565432108a174e2662aa5
Mahmoud Nasser <csemn.mail@gmail.com>
2022-03-31 01:56:30 +0200

    Update k_means.py

--- Cluster/k_means.py
+++ Cluster/k_means.py
@@ -2,3 +2,19 @@
 @author: CSEMN(Mahmoud Nasser)
 @since : 30 MAR 2022
 '''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+col_names=['timeStamp','Gender','Grade','Age','Length','Weight','ShoesSize']
+dataset = pd.read_csv("../human_features.csv",names= col_names,skiprows=(0,))
+
+#Pick Features and label
+x = dataset.iloc[:,5].values  # weight
+y = dataset.iloc[:, 4].values # length
+
+#Visualize data
+plt.scatter(x,y, label='True Position' )
+plt.xlabel("Weight")
+plt.ylabel("Lenght")
+plt.show()
