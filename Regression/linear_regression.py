87ee0b272256b4cd8a31aba8a779f260d9ae3230
Mahmoud Nasser <csemn.mail@gmail.com>
2022-03-30 23:35:59 +0200

    Update linear_regression.py

--- Regression/linear_regression.py
+++ Regression/linear_regression.py
@@ -2,4 +2,7 @@
 @author: CSEMN(Mahmoud Nasser)
 @since : 30 MAR 2022
 '''
-import numpy as np
+#Read Dataset from CSV file
+import pandas as pd
+col_names=['timeStamp','Gender','Grade','Age','Length','Weight','ShoesSize']
+dataset = pd.read_csv("../human_features.csv",names= col_names,skiprows=(0,))
