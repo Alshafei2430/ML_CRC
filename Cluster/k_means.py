f7f38702575597e5ab73e39338d4fff770d695bd
Mahmoud Nasser <csemn.mail@gmail.com>
2022-03-31 03:07:06 +0200

    k-means code finished
    1. pick features
    2. elbow method
    3. clustering
    4. visualizing

--- Cluster/k_means.py
+++ Cluster/k_means.py
@@ -9,12 +9,43 @@ import matplotlib.pyplot as plt
 col_names=['timeStamp','Gender','Grade','Age','Length','Weight','ShoesSize']
 dataset = pd.read_csv("../human_features.csv",names= col_names,skiprows=(0,))
 
-#Pick Features and label
-x = dataset.iloc[:,5].values  # weight
-y = dataset.iloc[:, 4].values # length
+#Pick Features
+x = dataset.iloc[:,[4,5]].values  # length, weight
 
 #Visualize data
-plt.scatter(x,y, label='True Position' )
-plt.xlabel("Weight")
-plt.ylabel("Lenght")
+plt.scatter(x[:,0],x[:,1])
+plt.xlabel("Lenght")
+plt.ylabel("Weight")
 plt.show()
+
+#Claculate best K using elbow method
+from sklearn.cluster import KMeans
+k_test_range = range(2,20)
+distorsions = []
+for k in k_test_range:
+    kmeans = KMeans(n_clusters=k)
+    kmeans.fit(x)
+    distorsions.append(kmeans.inertia_)
+
+from kneed import KneeLocator
+kneedle = KneeLocator(k_test_range,distorsions, S=1.0, curve="convex", direction="decreasing")
+print('Elbow boint : '+str(kneedle.elbow))
+kneedle.plot_knee()
+
+#Clustring
+kmeans = KMeans(n_clusters=kneedle.elbow)
+kmeans.fit(x)
+
+df = pd.DataFrame({'Length': x[:,0], 'Weight': x[:,1],'Cluster': kmeans.labels_})
+
+#Visualize Clusters
+plt.scatter(x[:,0],x[:,1], c=kmeans.labels_, cmap='rainbow')
+plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], s=100,color='black',label='Centroids')
+plt.xlabel("Lenght")
+plt.ylabel("Weight")
+plt.legend()
+plt.show()
+
+
+
+
